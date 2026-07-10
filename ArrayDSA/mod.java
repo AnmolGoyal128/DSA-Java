d37aaa2d526b924c73b9e092a0d327fde5ed1df0
Anmol Goyal <anmolgoyal128@gmail.com>
2026-07-10 16:11:37 +0530

    DSA Java

---
+++ ArrayDSA/mod.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+
+public class mod {
+    ///hasmaap
+    static int mod(int [] arr ){
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int index =0;index<arr.length;index++){
+            
+        
+    }
+}
